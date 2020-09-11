470048cbcf5245a25fce5011e5ef3b20632187c9
Andrius Dalgeda <andriusdal@outlook.com>
2020-09-11 15:15:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ import time
 def WFMarket_search_ingame_seller():
     while True:
         try:
+            # User input
             print('Search for an item on warframe.market: ')
             search = input()
             item_url = 'https://warframe.market/items/' + search.replace(' ', '_')
 
             # Prints the search item for QoL
-            print('Searching for listings of ' + string.capwords(search) + '... (' + item_url + ')')
-            print('')
-
+            print('Searching for listings of ' + string.capwords(search) + '... (' + item_url + ')','\n')
+            
+            # Requests response from online_URL and parses json data
             online_URL = 'https://api.warframe.market/v1/items/' + search.replace(' ', '_') + '/orders'
             online_request = requests.get(online_URL)
             online_json = online_request.json()
@@ -27,29 +28,39 @@ def WFMarket_search_ingame_seller():
                 # Only prints the listings from users that are ingame and are selling the searched item
                 if val["user"]["status"] == "ingame" and val["order_type"] == "sell":
                     
+                    # Common values within both print statements
                     username = val["user"]["ingame_name"]
                     plat_price = int(val["platinum"])
                     quantity = int(val["quantity"])
                     profile_URL = 'https://warframe.market/profile/' + username
     
-                    # If 'mod_rank' is present in the online_json the item is assumed to be a mod
+                    # If 'mod_rank' is present in the online_json the item is assumed to be a 'mod'
                     if "mod_rank" in val:
                         rank = int(val["mod_rank"])
                         print(username, 'is selling ', str(quantity), 'at rank', str(rank), 'for', str(plat_price) , 'Platinum each.',' '*5,'(' + profile_URL + ')')
-
-                    # If an item is not a mod, it must be a regular 'item'
+                        
+                    # If an item is not a mod, its assumed to be a regular 'item'
                     else:
                         print(username, 'is selling', str(quantity), 'for', str(plat_price), 'Platinum each.',' '*5,'(' + profile_URL + ')')
 
             # Waits for 334ms before taking another request (limitation of 3 requests/second)
             print('')
             time.sleep(334/1000)
-            WFMarket_search_ingame_seller()
+            WFMarket_search_ingame_seller
 
         # If the item is not a valid listing on warframe.market a key error is returned
         except KeyError:
             print("Invalid Item - Please check the spelling or enter a valid (and tradable) Warframe item ('Frost Prime Systems', 'Axi L4 Intact', 'Dead Eye')")
             time.sleep(334/1000)
             WFMarket_search_ingame_seller()
-
+            
+        # Error checking for whether the API is down
+        except json.JSONDecodeError:
+            print('The warframe.market API is currently down, please try again later.')
+            break
+        
 WFMarket_search_ingame_seller()
+
+
+
+
